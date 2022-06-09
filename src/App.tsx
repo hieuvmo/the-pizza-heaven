62d0b21dcee0ee25ff7be09325b87a35e1841f5a
vietminhhieu <vietminhhieua5@gmail.com>
2022-06-09 17:20:22 +0700

    finshing set up project and feat/create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from "react";
-import ".//common/sass/App.scss";
+import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './/common/sass/App.scss';
+import { routerPath } from './common/config/router/router.path';
+import { routerList } from './common/config/router/router.routes';
+import { IRoute } from './common/types/RouterModel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Pizza Heaven</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {routerList.map((route: IRoute, index: number) => {
+          return <Route path={route.path} element={route.element} key={index} />;
+        })}
+        <Route path="*" element={<Navigate to={routerPath.common.HOME} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Logo/PizzaLogo.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import { routerPath } from '../../common/config/router/router.path';
+
+export const PizzaLogo = () => {
+  return (
+    <Typography
+      variant="h6"
+      noWrap
+      component="a"
+      href={routerPath.common.HOME}
+      sx={{
+        mr: 4,
+        display: { xs: 'none', md: 'flex' },
+        fontFamily: 'monospace',
+        fontWeight: 700,
+        letterSpacing: '.3rem',
+        color: 'inherit',
+        textDecoration: 'none',
+      }}
+    >
+      <img alt="Pizza Logo" width={48} src="https://thepizzaheaven.com/wp-content/uploads/2019/09/Untitled-1.png" />
+      <div className="ml-2">
+        <div className="text-xl tracking-normal">The Pizza Heaven</div>
+        <p className="text-sm font-light tracking-tight">Everything about pizza</p>
+      </div>
+    </Typography>
+  );
+};
